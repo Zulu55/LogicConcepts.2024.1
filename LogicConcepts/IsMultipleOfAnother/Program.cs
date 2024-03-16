1e6789e00d5c95cff3897af3f6315723bc351d8c
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-16 11:28:28 -0500

    Is multiple of another

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,15 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es múltiplo {b}");
+    }
+} while (true);
