d20de2f48a5bad2f0da29dd23efc67fa5129049d
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-17 21:34:13 -0500

    Discounts

---
+++ LogicConcepts/Discounts/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var desks = ConsoleExtension.GetInt("Número de escritorios: ");
+    var valueToPay = CalculateValue(desks);
+    Console.WriteLine($"El valor a pagar es..: {valueToPay:C2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+decimal CalculateValue(int desks)
+{
+    float discount;
+    if (desks < 5)
+    {
+        discount = 0.1f;
+    }
+    else if (desks < 10)
+    {
+        discount = 0.2f;
+    }
+    else
+    {
+        discount = 0.4f;
+    }
+
+    return desks * 650000M * (decimal)(1 - discount);
+}
+
+Console.WriteLine("Game Over.");
