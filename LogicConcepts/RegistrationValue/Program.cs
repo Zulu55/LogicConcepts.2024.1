7844b555e24aec975d5d7dfc7fe937deb9a8e017
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-17 21:56:47 -0500

    Registration value

---
+++ LogicConcepts/RegistrationValue/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var credits = ConsoleExtension.GetInt("Número de créditos...........: ");
+    var creditValue = ConsoleExtension.GetDecimal("Valor crédito................: ");
+    var stratum = ConsoleExtension.GetInt("Estrato del estudiante.......: ");
+
+    var registrationValue = CalculateRegistrationValue(credits, creditValue, stratum);
+    var subsidy = CalculateSubsidy(stratum);
+
+    Console.WriteLine($"Costo de la matricula........: {registrationValue,20:C2}");
+    Console.WriteLine($"Valor del subsidio...........: {subsidy,20:C2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
+
+decimal CalculateSubsidy(int stratum)
+{
+    if (stratum == 1)
+    {
+        return 200000m;
+    }
+    if (stratum == 2)
+    {
+        return 100000m;
+    }
+    return 0;
+}
+
+decimal CalculateRegistrationValue(int credits, decimal creditValue, int stratum)
+{
+    decimal value;
+    if (credits <= 20)
+    {
+        value = credits * creditValue;
+    }
+    else
+    {
+        value = 20 * creditValue + (credits - 20) * creditValue * 2;
+    }
+
+    if (stratum == 1)
+    {
+        return value * 0.2m;
+    }
+    if (stratum == 2)
+    {
+        return value * 0.5m;
+    }
+    if (stratum == 3)
+    {
+        return value * 0.7m;
+    }
+
+    return value;
+}
+
