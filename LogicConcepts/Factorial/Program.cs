5d72a33ae5ceddc6c0d70df056a4b7a306b6ed5a
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-25 19:10:56 -0500

    Factrorial

---
+++ LogicConcepts/Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingrese número: ");
+    var factorial = MyMath.Factorial(n);
+    Console.WriteLine($"{n}! = {factorial:N0}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
---
+++ LogicConcepts/Shared/MyMath.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared
+{
+    public class MyMath
+    {
+        public static double Factorial(int n)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
