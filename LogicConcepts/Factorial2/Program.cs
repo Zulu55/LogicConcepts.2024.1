2b04a43c55278adc6ef7330c2b64766459ce0c73
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-04-13 10:32:37 -0500

    Factorial version 2.0

---
+++ LogicConcepts/Factorial2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.Clear();
+    Console.WriteLine("*** FACTORIAL ***");
+    var num = ConsoleExtension.GetInt("Ingrese el número al cual le deseas calcular el factorial: ");
+    var fac = MyMath.Factorial(num);
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"El factorial de: {num} es: {fac:N0}");
+    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+//double Factorial(int num)
+//{
+//    var i = 2;
+//    double fac = 1;
+//    while (i <= num)
+//    {
+//        fac *= i;
+//        i++;
+//    }
+//    return fac;
+//}
